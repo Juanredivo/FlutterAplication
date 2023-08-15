3ce8db0d88520074c9d1119d56faf9a140778274
Juan Redivo <juanredivo@hotmail.com>
2023-08-14 21:36:29 -0300

    Criando tela para acessar atividades

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,44 @@
 import 'package:flutter/material.dart';
-
+import 'atividade1.dart';
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _explorationMode = false;
-
-  void _toggleExplorationMode() {
-    setState(() {
-      _explorationMode = true;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('TrafficLife'),
+          title: const Text('Lista de atividades mobile'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Icon(
-                Icons.traffic,
-                size: 100,
-                color: Colors.blue,
-              ),
               SizedBox(height: 20),
               Text(
-                'Bem-vindo ao TrafficLife!',
+                'Escolha a atividade!',
                 style: TextStyle(fontSize: 20),
               ),
               SizedBox(height: 20),
               ElevatedButton(
-                onPressed: _toggleExplorationMode,
-                child: Text('Explorar'),
+                onPressed: () {
+
+                },
+                child: Text('Atividade 1'),
               ),
-              if (_explorationMode) ...[
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => ConsultarMultasScreen()));
-                  },
-                  child: Text('Consultar Multas'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => VeiculosVinculadosScreen()));
-                  },
-                  child: Text('Veículos Vinculados'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => DescontosEspeciaisScreen()));
-                  },
-                  child: Text('Descontos Especiais'),
-                ),
-              ],
+              ElevatedButton(
+                onPressed: () {
+
+                },
+                child: Text('Atividade 2'),
+              ),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class ConsultarMultasScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Consultar Multas'),
-      ),
-      body: Center(
-        child: Text('Tela de Consultar Multas'),
-      ),
-    );
-  }
-}
-
-class VeiculosVinculadosScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Veículos Vinculados'),
-      ),
-      body: Center(
-        child: Text('Tela de Veículos Vinculados'),
-      ),
-    );
-  }
-}
-
-class DescontosEspeciaisScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Descontos Especiais'),
-      ),
-      body: Center(
-        child: Text('Tela de Descontos Especiais'),
       ),
     );
   }
